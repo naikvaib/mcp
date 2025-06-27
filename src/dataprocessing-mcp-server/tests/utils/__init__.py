1cf30e049faa232b8f7c1f50ce679d46bb77946d
LiyuanLD <linliyu@amazon.com>
2025-06-26 21:01:59 -0700

    fix(dataprocessing-mcp-server): add pyrightconfig.json to ignore attribute access type check andadd more unit test for test coverage

--- src/dataprocessing-mcp-server/tests/utils/__init__.py
+++ src/dataprocessing-mcp-server/tests/utils/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for the Data Processing MCP Server models."""
+"""Tests for the utils module."""
